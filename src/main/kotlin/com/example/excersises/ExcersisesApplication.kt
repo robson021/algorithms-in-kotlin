99b6f4885546c59c34bc8cb2871a484d7050a25f
Robert Nowak <robertn@amazon.com>
2024-02-17 16:49:41 +0100

    initial commit

---
+++ src/main/kotlin/com/example/excersises/ExcersisesApplication.kt
@@ -0,0 +1,11 @@
+package com.example.excersises
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class ExcersisesApplication
+
+fun main(args: Array<String>) {
+	runApplication<ExcersisesApplication>(*args)
+}
---
+++ src/test/kotlin/com/example/excersises/ExcersisesApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.example.excersises
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class ExcersisesApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
